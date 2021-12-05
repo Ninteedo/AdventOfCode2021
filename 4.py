b1bc44860dcd4693f2aacaa4812519a4a35f4627
Richard Holmes <richard.gordon.holmes@gmail.com>
2021-12-05 12:38:27 +0000

    Use more boolean list evaluators in 4

--- 4.py
+++ 4.py
@@ -13,7 +13,7 @@ def applyBingoDraw(draw: int, boards: List[Board], boardMarks: List[BoardMarks])
     return [ [ [ boardMarks[i][k][j] or boards[i][k][j] == draw for j in range(0, boardSize) ] for k in range(0, boardSize) ] for i in range(0, len(boards)) ]
 
 def testBingo(boardMarks: BoardMarks) -> bool:
-    return any([ sum(boardMarks[rowNum]) == boardSize for rowNum in range(0, boardSize) ] + [ sum([ boardMarks[i][colNum] for i in range(0, boardSize) ]) == boardSize for colNum in range(0, boardSize) ])
+    return any([ all(boardMarks[rowNum]) for rowNum in range(0, boardSize) ]) or any([ all([ boardMarks[i][colNum] for i in range(0, boardSize) ]) for colNum in range(0, boardSize) ])
 
 def calculateUnmarkedSum(board: Board, markBoard: BoardMarks) -> int:
     return sum([ board[j][k] for j in range(0, boardSize) for k in range(0, boardSize) if not markBoard[j][k] ])
@@ -34,14 +34,14 @@ def part2(draws: List[int], boards: List[Board]) -> int:
         for i in range(0, len(boards)):
             if remainingBoards[i] and testBingo(boardMarks[i]):
                 remainingBoards[i] = False
-                if sum(remainingBoards) == 0:
+                if not any(remainingBoards):
                     return draw * calculateUnmarkedSum(boards[i], boardMarks[i])
 
 def main():
     lines = readLines(4)
 
-    draws = [ int(x.replace("\n", "")) for x in lines[0].split(",") if x != "" ]
-    boards = [ [ [ int(x.replace("\n", "")) for x in lines[i+j].split(" ") if x != "" ] for j in range(0, boardSize) ] for i in range(2, len(lines), boardSize+1) ]
+    draws = [ int(x) for x in lines[0].replace("\n", "").split(",") if x != "" ]
+    boards = [ [ [ int(x) for x in lines[i+j].replace("\n", "").split(" ") if x != "" ] for j in range(0, boardSize) ] for i in range(2, len(lines), boardSize+1) ]
 
     print("Part 1", part1(draws, boards))
     print("Part 2", part2(draws, boards))
