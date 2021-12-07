89afb9e3016a1fd5e8f871b2d56332caa917d7ae
Richard Holmes <richard.gordon.holmes@gmail.com>
2021-12-07 09:55:50 +0000

    Simplify day 1

--- 01.py
+++ 01.py
@@ -1,27 +1,19 @@
 from typing import List
 from input_reader import readLinesInt
 
-def increaseCount(xs: List[int]) -> int:
+def part1(xs: List[int]) -> int:
     '''O(n)'''
-    result = 0
-    for i in range(1, len(xs)):
-        if xs[i] > xs[i-1]:
-            result += 1
-    return result
+    return sum([ xs[i] > xs[i-1] for i in range(1, len(xs)) ])
 
-def slidingIncreaseCount(xs: List[int]) -> int:
+def part2(xs: List[int]) -> int:
     '''O(n)'''
-    result = 0
-    for i in range(3, len(xs)):
-        if (xs[i] + xs[i-1] + xs[i-2]) > (xs[i-1] + xs[i-2] + xs[i-3]):
-            result += 1
-    return result
+    return sum([ xs[i] > xs[i-3] for i in range(3, len(xs)) ])
 
 def main():
     xs = readLinesInt("01")
 
-    print("Part 1", increaseCount(xs))
-    print("Part 2", slidingIncreaseCount(xs))
+    print("Part 1", part1(xs))
+    print("Part 2", part2(xs))
 
 if __name__ == "__main__":
     main()
