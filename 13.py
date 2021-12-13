d729dd3069bcd82af933e75ebee0e1a048bdf607
Richard Holmes <richard.gordon.holmes@gmail.com>
2021-12-13 12:28:56 +0000

    Add runtime comments and remove unnecessary lines

--- 13.py
+++ 13.py
@@ -3,28 +3,27 @@ from functools import reduce
 from input_reader import readLines
 
 def part1(dots: List[Tuple[int, int]], folds: List[Tuple[str, int]]) -> int:
-    state = doFold(dots, folds[0])
-    uniques = set(state)
-    return len(uniques)
+    '''O(n)'''
+    return len(set(doFold(dots, folds[0])))
 
 def part2(dots: List[Tuple[int, int]], folds: List[Tuple[str, int]]) -> int:
-    state = reduce(doFold, folds, dots)
-    printBoard(state)
-    return "read 8 capital letters from printed board"
+    '''executing folds is O(mn) where m is number of folds, printing is O(maxX * maxY + n)'''
+    return "read 8 capital letters from printed board\n" + printBoard(reduce(doFold, folds, dots))
 
 def doFold(dots: List[Tuple[int, int]], fold: Tuple[str, int]) -> List[Tuple[int, int]]:
+    '''O(n)'''
     state = dots
-    xFold = fold[0] == "x"
     for i, dot in enumerate(state):
-        if xFold:   state[i] = (fold[1] - abs(dot[0] - fold[1]), dot[1])
-        else:       state[i] = (dot[0], fold[1] - abs(dot[1] - fold[1]))
+        if fold[0] == "x":  state[i] = (fold[1] - abs(dot[0] - fold[1]), dot[1])
+        else:               state[i] = (dot[0], fold[1] - abs(dot[1] - fold[1]))
     return state
 
-def printBoard(dots: List[Tuple[int, int]]) -> None:
-    maxX, maxY = max([ dot[0] for dot in dots ]), max([ dot[1] for dot in dots ])
+def printBoard(dots: List[Tuple[int, int]]) -> str:
+    '''O(maxX * maxY + n)'''
+    maxX, maxY = [ max([ dot[i] for dot in dots ]) for i in [0, 1] ]
     board = [ [ " " for _ in range(maxX+1) ] for _ in range(maxY+1) ]
     for dot in dots: board[dot[1]][dot[0]] = "#"
-    print("\n".join([ "".join(row) for row in board ]))
+    return "\n".join([ "".join(row) for row in board ])
 
 def main():
     lines = readLines("13")
