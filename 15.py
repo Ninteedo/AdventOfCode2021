c0cba328e671029663f1797eb996fe5682d83739
Richard Holmes <richard.gordon.holmes@gmail.com>
2021-12-15 13:34:48 +0000

    Use heapq instead of set to improve performance

--- 15.py
+++ 15.py
@@ -1,48 +1,44 @@
-from typing import List
+from typing import List, Tuple
 from input_reader import readLines
 
+from heapq import heappush, heappop
+
 def part1(xs: List[List[int]]) -> int:
-    '''Reasonable runtime <1s'''
     return a_star(xs)
 
 def part2(xs) -> int:
-    '''Terrible runtime ~20s'''
+    '''Bad runtime ~4s'''
     return a_star(expandMap(xs))
 
 def a_star(xs: List[List[int]]):
     # https://en.wikipedia.org/wiki/A*_search_algorithm
 
-    open = set([ (0, 0) ])
-
+    open = [ (-1, (0, 0)) ]
     g = [ [ -1 for _ in range(len(xs[0])) ] for _ in range(len(xs)) ]
     g[0][0] = 0
-    f = [ [ -1 for _ in range(len(xs[0])) ] for _ in range(len(xs)) ]
-    f[0][0] = xs[0][0]
-
+    target = (len(xs)-1, len(xs[0])-1)
     while len(open) > 0:
-        current = open.pop()    # can't get a random element from a set without popping it
-        open.add(current)
-        for o in open:
-            if f[o[0]][o[1]] < f[current[0]][current[1]]: current = o
-        if current == (len(xs)-1, len(xs[0])-1):
-            return g[current[0]][current[1]]
-        open.remove(current)
-
+        current = heappop(open)[1]
+        if current == target:
+            return gridGet(g, current)
         for r, c in [ (1, 0), (0, 1), (-1, 0), (0, -1) ]:
-            neighbour = (current[0]+r, current[1]+c)
-            if validCoord(xs, neighbour[0], neighbour[1]):
-                newG = g[current[0]][current[1]] + xs[neighbour[0]][neighbour[1]]
-                if newG < g[neighbour[0]][neighbour[1]] or g[neighbour[0]][neighbour[1]] == -1:
-                    g[neighbour[0]][neighbour[1]] = newG
-                    f[neighbour[0]][neighbour[1]] = newG + (1 + r + c) * 2  # heuristic, positive when moving down or right, negative otherwise
-                    if neighbour not in open: open.add(neighbour)
+            adj = (current[0]+r, current[1]+c)
+            if validCoord(xs, adj):
+                oldG, newG = gridGet(g, adj), gridGet(g, current) + gridGet(xs, adj)
+                if newG < oldG or oldG == -1:
+                    g[adj[0]][adj[1]] = newG
+                    h = r + c  # heuristic, positive when moving towards target, negative otherwise
+                    if adj not in open: heappush(open, (newG + h, adj))
     return "failure"
 
 def expandMap(xs: List[List[int]]) -> List[List[int]]:
     return [ [ (xs[r][c] + i + j - 1) % 9 + 1 for j in range(5) for c in range(len(xs)) ] for i in range(5) for r in range(len(xs)) ]
 
-def validCoord(xs: List[List[int]], row: int, col: int) -> bool:
-    return row >= 0 and row < len(xs) and col >= 0 and col < len(xs[0])
+def validCoord(xs: List[List[int]], coord: Tuple[int, int]) -> bool:
+    return coord[0] >= 0 and coord[0] < len(xs) and coord[1] >= 0 and coord[1] < len(xs[0])
+
+def gridGet(xs: List[List[int]], coord: Tuple[int, int]) -> int:
+    return xs[coord[0]][coord[1]]
 
 def main():
     lines = readLines("15")
