1de5444fbca2ac3b887dd4b7b9f582fde14e2782
Richard Holmes <richard.gordon.holmes@gmail.com>
2021-12-17 14:04:20 +0000

    More maths

--- 17.py
+++ 17.py
@@ -5,17 +5,18 @@ def part1(yMin: int) -> int:
     return (yMin * (yMin+1)) // 2
 
 def part2(xMin: int, xMax: int, yMin: int, yMax: int) -> int:
-    return sum([ sum([ hitsTarget(xVel, yVel, xMin, xMax, yMin, yMax) for xVel in range(0, xMax+1) ]) for yVel in range(yMin, abs(yMin)) ])
+    return sum([ sum([ hitsTarget(xVel, yVel, xMin, xMax, yMin, yMax) for xVel in range(xMax+1) ]) for yVel in range(yMin, -yMin) ])
 
 def hitsTarget(xVel: int, yVel: int, xMin: int, xMax: int, yMin: int, yMax: int) -> bool:
-    x, y, steps = 0, 0, 0
-    while y >= yMin and x <= xMax:
-        x += max(xVel - steps, 0)
-        y += yVel - steps
-        steps += 1
+    for steps in range(round(stepsToY(yMax, yVel)), round(stepsToY(yMin, yVel))+1):
+        x = ((xVel * (xVel+1)) // 2) - (max(xVel-steps, 0) * ((max(xVel-steps, 0)+1)) // 2)
+        y = (steps * yVel) - ((steps * (steps-1)) // 2)
         if x >= xMin and x <= xMax and y >= yMin and y <= yMax:
             return True
     return False
+
+def stepsToY(y, yVel):
+    return ((4*(yVel**2) + 4*yVel - 8*y + 1) ** 0.5 + 2*yVel + 1) / 2
 
 def main():
     lines = readLines("17")
